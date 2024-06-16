697190280933d260fb06e0407f42795ec8d16ab7
Chuckshallcode <charleycooling@gmail.com>
2024-06-15 22:19:11 -0500

    first commit

---
+++ mysql.java/src/main/java/recipes/Recipes.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package recipes;
+
+import java.sql.Connection;
+
+import recipes.dow.DbConnection;
+/**
+ * 
+ */
+public class Recipes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DbConnection.getConnection();
+	}
+
+}
